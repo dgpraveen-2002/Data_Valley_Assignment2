5326cd1397b9ca9f65a308721f53555d2af7de0f
dgpraveen-2002 <115911551+dgpraveen-2002@users.noreply.github.com>
2024-03-13 18:06:41 +0530

    Update Divisible_By_2_and_5.java

--- Divisible_By_2_and_5.java
+++ Divisible_By_2_and_5.java
@@ -1,11 +1,11 @@
 public class Divisible_By_2_and_5{
     public static void main(String[] args) {
         int[] values = {100, 52, 43, 99, 42, 40, 91};
-        int sum = findSumDivisibleByFiveAndTwo(values);
+        int sum = findSum(values);
         System.out.println("Sum of numbers divisible by both 5 and 2: " + sum);
     }
     
-    public static int findSumDivisibleByFiveAndTwo(int[] arr) {
+    public static int findSum(int[] arr) {
         int sum = 0;
         for (int num : arr) {
             if (num % 5 == 0 && num % 2 == 0) {
